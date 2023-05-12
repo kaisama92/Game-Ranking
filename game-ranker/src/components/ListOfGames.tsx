e35f2998d08dd73c29a99ad1d673b55b9fed5810
Kai Clausen <kaiclausen123@gmail.com>
2023-05-12 11:58:16 -0700

    researching rawg api endpoints, taking break for lunch

--- game-ranker/src/components/ListOfGames.tsx
+++ game-ranker/src/components/ListOfGames.tsx
@@ -1,7 +1,43 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
+
+interface Game {
+  name: string;
+}
 
 const ListOfGames: React.FC = () : ReactElement => {
-  return <h2>Testing</h2>
+  const [gamesList, setGamesList] = useState<Game[]>([]);
+
+  // useEffect(() => {
+  //   fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&platform=4&page_size=10001`)
+  //       .then(response => response.json())
+  //       .then((jsonifiedResponse) => {
+  //         setGamesList(jsonifiedResponse.results);
+  //       });
+  // }, []);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      let currentPage = 1;
+      let totalPages = 1;
+      let results: Game [] = [];
+
+      while (currentPage <= totalPages) {
+        const response = await fetch()
+      }
+    }
+  })
+
+  return (
+    <React.Fragment>
+      <ul>
+        {gamesList.map((game, index) => (
+          <li key={index}>
+            <h3>{game.name}</h3>
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  )
 }
 
 export default ListOfGames;
