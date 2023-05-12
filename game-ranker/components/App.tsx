1bd0de706f76ac6a4e7a4d3f80a1d149a7ace1fe
Kai Clausen <kaiclausen123@gmail.com>
2023-05-12 08:52:30 -0700

    move app.tsx and restructure file.

---
+++ game-ranker/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
