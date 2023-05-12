8cb65a6ff5d0ff9daaae2279e78e59a9c91938bc
Kai Clausen <kaiclausen123@gmail.com>
2023-05-12 10:12:23 -0700

    restructure application

--- game-ranker/src/components/App.tsx
+++ game-ranker/src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./Header";
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import ListOfGames from "./ListOfGames";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <ListOfGames />
     </React.Fragment>
   );
 }
---
+++ game-ranker/src/components/ListOfGames.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+const ListOfGames: React.FC = () : ReactElement => {
+  return <h2>Testing</h2>
+}
+
+export default ListOfGames;
