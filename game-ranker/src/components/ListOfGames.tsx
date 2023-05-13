fd0123887de4e303ca11b07ad9000a1ef42ad93a
Kai Clausen <kaiclausen123@gmail.com>
2023-05-13 14:24:24 -0700

    add search functionality

--- game-ranker/src/components/ListOfGames.tsx
+++ game-ranker/src/components/ListOfGames.tsx
@@ -1,42 +1,67 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { FormEvent, FormEventHandler, ReactElement, useEffect, useState } from "react";
 import db from "../firebase";
 import { collection, addDoc } from "firebase/firestore";
 
 interface Game {
   name: string;
-  genre: [] | undefined
+  slug: string;
+}
+
+interface searchFor {
+  name: string;
+  slug: string;
+}
+
+interface search {
+  search: string;
 }
 
 const ListOfGames: React.FC = () : ReactElement => {
+  const urlBase: string = `https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&platform=4`
   const [gamesList, setGamesList] = useState<Game[]>([]);
+  const [searchList, setSearchList] = useState<searchFor[]>([]);
 
-  useEffect(() => {
-    fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&platform=4&page_size=40`)
+  // useEffect((): void => {
+  //   fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&platform=4&page_size=40`)
+  //       .then(response => response.json())
+  //       .then((jsonifiedResponse) => {
+  //         setGamesList(jsonifiedResponse.results);
+  //       });
+  // }, []);
+
+  const searchForGame = (query: string): void => {
+    fetch(`${urlBase}&search=${query}`)
         .then(response => response.json())
-        .then((jsonifiedResponse) => {
-          setGamesList(jsonifiedResponse.results);
-        });
-  }, []);
+        .then((jsonifiedResponse): void => {
+          setSearchList(jsonifiedResponse.results);
+        })
+  }
 
-  // useEffect(() => {
-  //   const fetchGames = async () => {
-  //     let currentPage = 1;
-  //     let totalPages = 1;
-  //     let results: Game [] = [];
-
-  //     while (currentPage <= totalPages) {
-  //       const response = await fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&platform=4&tags=multiplayer`)
-  //     }
-  //   }
-  // })
+  const handleFormSubmission = (event: FormEvent): void => {
+    event.preventDefault();
+    const {target} = event;
+    if (target) {
+      fetch(`${urlBase}&search=${(target as HTMLFormElement).search.value}&exclude_additions=true`)
+      .then(response => response.json())
+      .then((jsonifiedResponse): void => {
+        setSearchList(jsonifiedResponse.results);
+      })
+    }
+  } 
 
   return (
     <React.Fragment>
-      <ol>
-        {gamesList.map((game, index) => (
+      <form onSubmit={handleFormSubmission}>
+        <input 
+          type="text"
+          name="search"
+          placeholder="Search For Game Here"/>
+        <button type="submit">Search</button>
+      </form>
+      <ol>{searchList.map((game, index) => (
           <li key={index}>
-            <h3>{game.name}+{}</h3>
-            <button>Hello</button>
+            <h3>{game.name}
+            </h3>
           </li>
         ))}
       </ol>
