c770a9c0d07ff2dee54902636930d64b1f8f49f7
Kai Clausen <kaiclausen123@gmail.com>
2023-05-16 14:46:49 -0700

    researching modals for sign in

--- game-ranker/src/components/RegisterAccount.tsx
+++ game-ranker/src/components/RegisterAccount.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, FormEvent } from "react";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const registerForAccount : React.FC = () : ReactElement => {
@@ -8,19 +8,22 @@ const registerForAccount : React.FC = () : ReactElement => {
     const { target } = event;
     const auth = getAuth();
     if (target) {
-      createUserWithEmailAndPassword(auth, target.email, target.password)
+      createUserWithEmailAndPassword(auth, (target as HTMLFormElement).email.value, ( target as HTMLFormElement ).password.value)
           .then((userCredential) => {
             const user = userCredential.user;
           })
     }
   }
-
+ 
   return (
     <React.Fragment>
       <form onSubmit={handleRegistration} >
         <input type="email" name="email"/>
         <input type="text" name="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+" />
+        <button type="submit">Register</button>
       </form>
     </React.Fragment>
   )
 }
+
+export default registerForAccount;
