2d3f436207a3078596a2b0b838ea983842888e5b
Kai Clausen <kaiclausen123@gmail.com>
2023-05-18 14:12:22 -0700

    add functionality to keep users from upvoting and downvoting more than once

--- game-ranker/src/components/GameList.tsx
+++ game-ranker/src/components/GameList.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import {dbGameEntry} from "./Interfaces";
-import { collection, addDoc, onSnapshot, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { collection, onSnapshot, deleteDoc, doc, updateDoc } from "firebase/firestore";
 import db from "../firebase";
 
 interface gameListProps {
@@ -9,6 +9,8 @@ interface gameListProps {
 
 const GameList : React.FC<gameListProps> = ({user}) : ReactElement => {
   const [gameList, setGameList] = useState<dbGameEntry[]>([])
+  const [ error, setError ] = useState('');
+  const [ count, setCount ] = useState(0);
 
   useEffect(() => {
     const unSubscribe = onSnapshot(
@@ -22,6 +24,8 @@ const GameList : React.FC<gameListProps> = ({user}) : ReactElement => {
             upvotes: doc.data().upvotes,
             downvotes: doc.data().downvotes,
             metacritic: doc.data().metacritic,
+            hasDownvoted: doc.data().hasDownvoted,
+            hasUpvoted: doc.data().hasUpvoted,
             id: doc.id
           });
         });
@@ -34,44 +38,118 @@ const GameList : React.FC<gameListProps> = ({user}) : ReactElement => {
           }
           return 0;
         })
-        setGameList(games);
+        let uniqueArr: dbGameEntry[] = Array.from(new Set(games.map(a => a.slug)))
+          .map(slug => {
+            return games.find(a => a.slug === slug)!;
+          })
+        setGameList(uniqueArr);
       },
       (error) => {
         // do something with error
       }
     );
     return () => unSubscribe();
-  }, []);
+  }, [setCount]);
 
   const deleteEntry = async (id: string) => {
     await deleteDoc(doc(db, "games", id));
   }
 
-  const upvoteOrDownvoteEntry = async (name: string, slug: string, upvotes: number, downvotes: number, metacritic: number, id: string) => {
-    const gameToEdit = {
-      name: name,
-      slug: slug, 
-      upvotes: upvotes, 
-      downvotes: downvotes,
-      metacritic: metacritic,
-      id: id
+  
+  const upvoteEntry = async (name: string, slug: string, upvotes: number, downvotes: number, metacritic: number | string, hasUpvoted: string, hasDownvoted: string, id: string) => {
+    let upvoteName: string = hasUpvoted;
+    let downvoteName: string = hasDownvoted;
+    let addUpvote: number = 0;
+    if (hasDownvoted === '' || hasDownvoted === undefined) {
+      downvoteName = "None Have Upvoted"
+    } else {
+      downvoteName = hasDownvoted;
     }
+    if (hasUpvoted === '' || hasUpvoted === undefined || hasUpvoted === "None Have Downvoted") {
+      console.log("scenario1")
+      upvoteName = user;
+    } else if (hasUpvoted === "None Have Voted") {
+      addUpvote = 1;
+      upvoteName = user;
+    } else if (!hasUpvoted.includes(user) ){
+      addUpvote = 1;
+      upvoteName = hasUpvoted + user;
+    } else if (hasUpvoted.includes(user) || hasUpvoted === user ){
+      setError(`You have already upvoted ${name}`)
+    }
+    const gameToEdit = {
+    name: name,
+    slug: slug, 
+    upvotes: upvotes + addUpvote, 
+    downvotes: downvotes,
+    metacritic: metacritic,
+    hasUpvoted: upvoteName,
+    hasDownvoted: downvoteName,
+    id: id
+    } 
     const gameRef = doc(db!, "games"!, gameToEdit.id!);
-    await updateDoc(gameRef, gameToEdit);
-  }
+    await updateDoc(gameRef, gameToEdit)
+    setCount(count + 1);    
+}
+
+  
+  const downvoteEntry = async (name: string, slug: string, upvotes: number, downvotes: number, metacritic: number | string, hasUpvoted: string, hasDownvoted: string, id: string) => {
+    let upvoteName: string = hasUpvoted;
+    let downvoteName: string = hasDownvoted;
+    let addDownvote: number = 0;
+    console.log(hasDownvoted)
+    if (hasUpvoted === '' || hasUpvoted === undefined) {
+      upvoteName = "None Have Upvoted"
+    } else {
+      upvoteName = hasUpvoted;
+    }
+    if (hasDownvoted === '' || hasDownvoted === undefined || hasDownvoted === "None Have Downvoted") {
+      console.log("scenario1")
+      downvoteName = user;
+    } else if (hasDownvoted === "None Have Voted") {
+      console.log("scenario2");
+      addDownvote = 1;
+      downvoteName = user;
+    } else if (!hasDownvoted.includes(user) ){
+      console.log("scenario3");
+      addDownvote = 1;
+      downvoteName = hasDownvoted + user;
+    } else if (hasDownvoted.includes(user) || hasDownvoted === user ){
+      console.log("blocked");
+      setError(`You have already downvoted ${name}`)
+    }
+    const gameToEdit = {
+    name: name,
+    slug: slug, 
+    upvotes: upvotes, 
+    downvotes: downvotes + addDownvote,
+    metacritic: metacritic,
+    hasUpvoted: upvoteName,
+    hasDownvoted: downvoteName,
+    id: id
+    } 
+    const gameRef = doc(db!, "games"!, gameToEdit.id!);
+    await updateDoc(gameRef, gameToEdit)
+    setCount(count + 1);    
+    console.log(count);
+    console.log(error);
+
+}
+
   if (user !== "nfiZ9mjNwFYBEqcB7FRqNnvjx6j2") {
     return (
       <React.Fragment>
         <div className="toLeft">
           <h2 className="toCenter">Games to Consider</h2>
+          {error && <p className="error2">{error}</p>}
           <ul >
           {gameList.map((game,index) => (
             <li key= {index}>
               <h3>{game.name}</h3>
-              <p>{"Upvotes: " + game.upvotes + "  "}
-              <button onClick={() => upvoteOrDownvoteEntry(game.name, game.slug, (game.upvotes + 1), game.downvotes, game.metacritic, game.id!)} >Upvote</button></p>
-              <p>{"Downvotes: " + game.downvotes} <button onClick={() => upvoteOrDownvoteEntry(game.name, game.slug, game.upvotes, (game.downvotes + 1), game.metacritic, game.id!)} >Downvote</button></p>
-              <p>{"Metacritic Score: " + game.metacritic}</p>
+              <p className="gameText">{"Upvotes: " + game.upvotes + "  "}
+              <button onClick={() => upvoteEntry(game.name, game.slug, (game.upvotes + 1), game.downvotes, game.metacritic, game.hasUpvoted!, game.hasDownvoted!, game.id!)} >Upvote</button></p>
+              <p className="gameText">{"Downvotes: " + game.downvotes} <button onClick={() => downvoteEntry(game.name, game.slug, game.upvotes, (game.downvotes), game.metacritic, game.hasUpvoted!, game.hasDownvoted!, game.id!)} >Downvote</button></p>
+              <p className="gameText">{"Metacritic Score: " + game.metacritic}</p>
             </li>
           ))}
           </ul>
@@ -83,18 +161,19 @@ const GameList : React.FC<gameListProps> = ({user}) : ReactElement => {
       <React.Fragment>
         <div className="toLeft">
           <h2 className="toCenter">Games to Consider</h2>
-          <ul>
+          {error && <p className="error2">{error}</p>}
+          <ol>
             {gameList.map((game,index) => (
               <li key= {index}>
                 <h3>{game.name}</h3>
-                <p>{"Upvotes: " + game.upvotes + "  "}
-                <button onClick={() => upvoteOrDownvoteEntry(game.name, game.slug, (game.upvotes + 1), game.downvotes, game.metacritic, game.id!)} >Upvote</button></p>
-                <p>{"Downvotes: " + game.downvotes} <button onClick={() => upvoteOrDownvoteEntry(game.name, game.slug, game.upvotes, (game.downvotes + 1), game.metacritic, game.id!)} >Downvote</button></p>
-                <p>{"Metacritic Score: " + game.metacritic}</p>
+                <p className="gameText">{"Upvotes: " + game.upvotes + "  "}
+                <button onClick={() => upvoteEntry(game.name, game.slug, (game.upvotes), game.downvotes, game.metacritic, game.hasUpvoted!, game.hasDownvoted!, game.id!)} >Upvote</button></p>
+                <p className="gameText">{"Downvotes: " + game.downvotes} <button onClick={() => downvoteEntry(game.name, game.slug, game.upvotes, game.downvotes, game.metacritic, game.hasUpvoted!, game.hasDownvoted!, game.id!)} >Downvote</button></p>
+                <p className="gameText">{"Metacritic Score: " + game.metacritic}</p>
                 <button onClick={() => deleteEntry(game.id!)} >Delete</button>
               </li>
             ))}
-          </ul>
+          </ol>
         </div>
       </React.Fragment>
     )
