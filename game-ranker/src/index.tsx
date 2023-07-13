bd75882166294b9c699c73752954520b56067471
Kai Clausen <kaiclausen123@gmail.com>
2023-07-12 22:08:42 -0700

    update telemetry code
    Co-Authored by: Douglas Cook

--- game-ranker/src/index.tsx
+++ game-ranker/src/index.tsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+// import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+// import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+// var faro = initializeFaro({
+//   url: 'https://faro-collector-prod-us-east-0.grafana.net/collect/11abbc1dd4b1246799efec79afc7e6be',
+//   app: {
+//     name: 'Group Game Rankings',
+//     version: '1.0.0',
+//     environment: 'production'
+//   },
+//   instrumentations: [
+//     // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
+//     ...getWebInstrumentations(),
+
+//     // Initialization of the tracing package.
+//     // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
+//     new TracingInstrumentation(),
+//   ],
+// });
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
